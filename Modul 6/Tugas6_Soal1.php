382645f97ec90be2dcb16a34aff994ffe79a2798
Raka Ryaudi Nalda Nata Dikarta <91064845+rakaryaudi@users.noreply.github.com>
2022-04-09 12:20:11 +0700

    Add files via upload

---
+++ Modul 6/Tugas6_Soal1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+<?php 
+echo "<b>Mengunakan String Function :</b><br>";
+$string = 'D3 Rekayasa Perangkat Lunak Aplikasi';
+echo "<br>".substr($string, 3);
+echo "<br>".substr($string, -8);
+echo "<br>".substr($string, 3, 24);
+
+
+?>
+</body>
+</html>
